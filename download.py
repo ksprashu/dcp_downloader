fc9af43358e63ab3ba0877f2c37f879c2b56f9f8
Prashanth K S <ksprashanth82@gmail.com>
2021-02-10 08:39:46 +0530

    add main file

---
+++ download.py
@@ -0,0 +1,13 @@
+# This module reads DCP emails on GMail and then downloads all problem solution pairs
+
+from absl import app
+from absl import flags
+
+
+def main(argv: Sequence[str]) -> None:
+    del argv
+    pass
+
+
+if __name__ == '__main__':
+    app.run(main)
