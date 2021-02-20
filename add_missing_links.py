e74d1c4437a8caed6ca4917b585fd8bfff059ecb
Prashanth K S <ksprashanth82@gmail.com>
2021-02-20 08:28:04 +0530

    dedupe while saving back added links

--- add_missing_links.py
+++ add_missing_links.py
@@ -15,10 +15,13 @@ def main(argv: Sequence[str]) -> None:
 
     run_data = download_helper.get_run_data()
     links = run_data.get('links', [])
+
     if links:
         logging.info('Adding %d links', len(MISSING_LINKS))
         links.extend(MISSING_LINKS)
         links = list(set(links))
+        run_data['links'] = links
+        logging.info('Writing %d links', len(links))
         download_helper.save_run_data(run_data)
 
 
