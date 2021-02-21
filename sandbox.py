ade065c963551832d777e9fa98494db75a6b5cdc
Prashanth K S <ksprashanth82@gmail.com>
2021-02-21 15:13:53 +0530

    updated sandbox app

--- sandbox.py
+++ sandbox.py
@@ -3,7 +3,9 @@
 
 import base64
 
+import dcp_service
 import download_helper
+import html_service
 
 from absl import app
 
@@ -12,6 +14,7 @@ def main(argv):
     gmail_svc = download_helper.init_and_get_gmail_service()
 
     ids, _ = gmail_svc.search_messages('Subject:(Daily Coding Problem: Problem #761)')
+    # ids, _ = gmail_svc.search_messages('Subject:(Daily Coding Problem: Solutions to Previous Problems)')
     print(ids)
 
     msg = gmail_svc.get_message_content(ids[0])
@@ -26,9 +29,20 @@ def main(argv):
     print('Parts')
     print('There are %d parts' % len(msg['parts']))
 
-    for part in msg['parts']:
-        print(part['mimeType'])
-        print(base64.urlsafe_b64decode(part['body']['data']))
+    # for part in msg['parts']:
+    #     print(part['mimeType'])
+    #     print(base64.urlsafe_b64decode(part['body']['data']))
+
+    dcp_svc = dcp_service.DCP_Service(gmail_svc)
+    txt_msg = dcp_svc.get_text_message(ids[0])
+    # print(txt_msg)
+
+    links = dcp_svc.get_solution_links_from_text(txt_msg)
+
+    html_svc = html_service.Html_Service()
+    for link in links:
+        api_link = html_svc.get_api_link_from_href(link)
+        print(html_svc.get_api_content_as_md(api_link))
 
 
 if __name__ == '__main__':
